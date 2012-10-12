d74093b90665af3ccd4f8132512288ad8136fded
Kelsey Martens <kelsey@solspace.com>
2012-10-11 19:47:52 -0500

    Initial commit

---
+++ language/english/lang.comment_uploads.php
@@ -0,0 +1,55 @@
+<?php
+
+ /**
+ * Solspace - Comment Uploads
+ *
+ * @package		Solspace:Comment Uploads
+ * @author		Solspace DevTeam
+ * @copyright	Copyright (c) 2008-2012, Solspace, Inc.
+ * @link		http://solspace.com/docs/addon/c/Comment_Uploads/
+ * @version		2.0.1
+ * @filesource 	./system/expressionengine/third_party/comment_uploads/language/english/
+ */
+
+$L = array(
+	
+'comment_uploads_module_name' => 
+'Comment Uploads',
+
+'comment_uploads_module_description' =>
+'Allows your users to upload/attach files or images to EE comments.',
+
+'upload_failed'		=>
+'The upload did not complete successfully. The server said: ',
+
+'file_exceeds_ini_limit'	=>
+'The file is larger than this site\'s settings allow.',
+
+'file_partially_uploaded'	=>
+'The file was only partially uploaded.',
+
+'no_file_selected' =>
+'No file was selected to be uploaded.',
+
+'file_upload_error'	=>
+'"File upload error". Sorry, no more details are available.',
+
+'invalid_filetype'	=>
+'The file type you tried to upload is not allowed.',
+
+'invalid_filesize'	=>
+'The file size is too big.',
+
+'invalid_dimensions'	=>
+'The image\'s dimensions are invalid.',
+
+'upload_error'	=>
+'"Upload error". Something happened while moving the file to its new home.',
+
+'invalid_filecontent'	=>
+'PHP thinks this file is not the type of file it claims to be.',
+
+'' => ''
+);
+
+?>
